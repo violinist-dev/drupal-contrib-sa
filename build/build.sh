c28370d9289494eb7754f4aac933fa438daa57cf
Eirik Stanghelle Morland <eirik@morland.no>
2022-02-27 22:14:04 +0100

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -10,4 +10,5 @@ rm -rf sa_yaml/7/drupal sa_yaml/8/drupal
 php application.php drupal-contrib-sa:download > /dev/null
 php application.php drupal-contrib-sa:complete > /dev/null
 
-git diff --no-renames --name-only --diff-filter=D -z
+# Do not commit deletions.
+git diff --no-renames --name-only --diff-filter=D -z | xargs -0 git checkout --
