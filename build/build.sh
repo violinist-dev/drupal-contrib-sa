c1617d7e74b6c0b854b33effc2a99dc5d3e122df
Eirik Stanghelle Morland <eirik@morland.no>
2022-02-27 22:20:28 +0100

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -15,3 +15,7 @@ git diff --no-renames --name-only --diff-filter=D -z | xargs -0 git checkout --
 
 # These have crap version names.
 git checkout sa_yaml/8/drupal/svg_formatter/sa-contrib-2018-027.yaml
+git checkout sa_yaml/8/drupal/domain_group/sa-contrib-2021-037.yaml
+
+# This one is manually added as a core exclusion special case.
+rm sa_yaml/8/drupal/quickedit/sa-contrib-2022-025.yaml
